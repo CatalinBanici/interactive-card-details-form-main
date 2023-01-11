d9714deedd632a5e5a5277b916c157fba2a4ab1f
Catalin Banici <catalinbanici95@yahoo.com>
2023-01-11 20:23:29 +0100

    attempting something crazy

--- app/src/App.js
+++ app/src/App.js
@@ -1,29 +1,13 @@
 import Form from "./components/Form";
 import Main from "./components/Main";
-import { useState } from "react";
 
 function App() {
 
-  const [inputValues, setInputValues] = useState('');
-
-  function handleChange(e) {
-    setInputValues({...inputValues, [e.target.name]: e.target.value});
-  }
-
-  function handleSubmit(e) {
-    e.preventDefault();
-  }
-
-  console.log(inputValues)
-  
-
   return (
     <main>
-      <Main inputValues={inputValues} />
-      <Form handleChange={handleChange} handleSubmit={handleSubmit}  />
-      <span>{}</span>
+      <Main />
+      <Form />
     </main>
-    
   );
 }
 
--- app/src/components/Form.js
+++ app/src/components/Form.js
@@ -1,21 +1,41 @@
 import React from 'react';
 import '../index.css';
+import { useState } from 'react';
 
-export default function Form(props) {
+export default function Form() {
 
-  const {handleChange, handleSubmit} = props;
+  const [cardName, setCardName] = useState('');
+  const [cardNumber, setCardNumber] = useState('');
+  const [expDateM, setExpDateM] = useState('');
+  const [expDateY, setExpDateY] = useState('');
+  const [cvc, setCvc] = useState('');
 
+
+  function handleSubmit(e) {
+    e.preventDefault();
+  }
+  
   return (
     <form className='form-container' onSubmit={handleSubmit} >
       <label className='cardholder-name label'>
         CARDHOLDER NAME
-        <input type='text' name='card-name' required onChange={handleChange} />
+        <input 
+          type='text' 
+          name='card-name' 
+          required 
+          value={cardName}
+          onChange={(e) => setCardName(e.target.value)}  />
         <span className='error-message'>Can't be blank!</span>
       </label>
 
       <label className='card-number label'>
         CARD NUMBER
-        <input type='text' name='card-number' required onChange={handleChange}  />
+        <input 
+          type='text' 
+          name='card-number' 
+          required 
+          value={cardNumber}
+          onChange={(e) => setCardNumber(e.target.value)}  />
         <span className='error-message'>Can't be blank!</span>
         <span className='error-message-wrong'>Wrong format, numbers only!</span>
       </label>
@@ -24,8 +44,18 @@ export default function Form(props) {
         <label className='card-exp label'>
             EXP. DATE (MM/YY)
             <div>
-              <input type='text' name='month-expiration' required onChange={handleChange} />
-              <input type='text' name='year-expiration' required onChange={handleChange} /> 
+              <input 
+                type='text' 
+                name='month-expiration' 
+                required 
+                value={expDateM}
+                onChange={(e) => setExpDateM(e.target.value)} />
+              <input 
+                type='text' 
+                name='year-expiration' 
+                required 
+                value={expDateY}
+                onChange={(e) => setExpDateY(e.target.value)} /> 
               <span className='error-message'>Can't be blank!</span> 
             </div>
             
@@ -33,11 +63,17 @@ export default function Form(props) {
         
         <label className='cvc label'>
             CVC
-            <input type='text' name='cvc' required onChange={handleChange} />
+            <input
+              type='text' 
+              name='cvc' 
+              required 
+              value={cvc}
+              onChange={(e) => setCvc(e.target.value)} />
             <span className='error-message'>Can't be blank!</span>
         </label>       
       </div>
       <button>Confirm</button>
+      <span>{cardName}</span>
     </form>
   )
 }
